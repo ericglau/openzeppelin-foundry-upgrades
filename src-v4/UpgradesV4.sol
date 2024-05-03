33c5b86dd16b9f01d0d24488106e28679ffb31a9
Eric Lau <ericglau@outlook.com>
2024-05-03 15:28:23 -0400

    Use profiles for V4 - WIP

---
+++ src-v4/UpgradesV4.sol
@@ -0,0 +1,238 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Options} from "../src/Options.sol";
+import {Versions} from "../src/internal/Versions.sol";
+import {Utils} from "../src/internal/Utils.sol";
+import {DefenderDeploy} from "../src/internal/DefenderDeploy.sol";
+import {IUpgradeableProxy} from "../src/internal/interfaces/IUpgradeableProxy.sol";
+import {IProxyAdmin} from "../src/internal/interfaces/IProxyAdmin.sol";
+
+import {ValidateAndUpgrade} from "../src/internal/ValidateAndUpgrade.sol";
+
+/**
+ * @dev Library for deploying and managing upgradeable contracts from Forge scripts or tests.
+ */
+library UpgradesV4 {
+    /**
+     * @dev Upgrades a proxy to a new implementation contract. Only supported for UUPS or transparent proxies.
+     *
+     * Requires that either the `referenceContract` option is set, or the new implementation contract has a `@custom:oz-upgrades-from <reference>` annotation.
+     *
+     * @param proxy Address of the proxy to upgrade
+     * @param contractName Name of the new implementation contract to upgrade to, e.g. "MyContract.sol" or "MyContract.sol:MyContract" or artifact path relative to the project root directory
+     * @param data Encoded call data of an arbitrary function to call during the upgrade process, or empty if no function needs to be called during the upgrade
+     * @param opts Common options
+     */
+    function upgradeProxy(address proxy, string memory contractName, bytes memory data, Options memory opts) internal {
+        ValidateAndUpgrade.upgradeProxy(proxy, contractName, data, opts);
+    }
+
+    /**
+     * @dev Upgrades a proxy to a new implementation contract. Only supported for UUPS or transparent proxies.
+     *
+     * Requires that either the `referenceContract` option is set, or the new implementation contract has a `@custom:oz-upgrades-from <reference>` annotation.
+     *
+     * @param proxy Address of the proxy to upgrade
+     * @param contractName Name of the new implementation contract to upgrade to, e.g. "MyContract.sol" or "MyContract.sol:MyContract" or artifact path relative to the project root directory
+     * @param data Encoded call data of an arbitrary function to call during the upgrade process, or empty if no function needs to be called during the upgrade
+     */
+    function upgradeProxy(address proxy, string memory contractName, bytes memory data) internal {
+        Options memory opts;
+        ValidateAndUpgrade.upgradeProxy(proxy, contractName, data, opts);
+    }
+
+    /**
+     * @notice For tests only. If broadcasting in scripts, use the `--sender <ADDRESS>` option with `forge script` instead.
+     *
+     * @dev Upgrades a proxy to a new implementation contract. Only supported for UUPS or transparent proxies.
+     *
+     * Requires that either the `referenceContract` option is set, or the new implementation contract has a `@custom:oz-upgrades-from <reference>` annotation.
+     *
+     * This function provides an additional `tryCaller` parameter to test an upgrade using a specific caller address.
+     * Use this if you encounter `OwnableUnauthorizedAccount` errors in your tests.
+     *
+     * @param proxy Address of the proxy to upgrade
+     * @param contractName Name of the new implementation contract to upgrade to, e.g. "MyContract.sol" or "MyContract.sol:MyContract" or artifact path relative to the project root directory
+     * @param data Encoded call data of an arbitrary function to call during the upgrade process, or empty if no function needs to be called during the upgrade
+     * @param opts Common options
+     * @param tryCaller Address to use as the caller of the upgrade function. This should be the address that owns the proxy or its ProxyAdmin.
+     */
+    function upgradeProxy(
+        address proxy,
+        string memory contractName,
+        bytes memory data,
+        Options memory opts,
+        address tryCaller
+    ) internal {
+        ValidateAndUpgrade.upgradeProxy(proxy, contractName, data, opts, tryCaller);
+    }
+
+    /**
+     * @notice For tests only. If broadcasting in scripts, use the `--sender <ADDRESS>` option with `forge script` instead.
+     *
+     * @dev Upgrades a proxy to a new implementation contract. Only supported for UUPS or transparent proxies.
+     *
+     * Requires that either the `referenceContract` option is set, or the new implementation contract has a `@custom:oz-upgrades-from <reference>` annotation.
+     *
+     * This function provides an additional `tryCaller` parameter to test an upgrade using a specific caller address.
+     * Use this if you encounter `OwnableUnauthorizedAccount` errors in your tests.
+     *
+     * @param proxy Address of the proxy to upgrade
+     * @param contractName Name of the new implementation contract to upgrade to, e.g. "MyContract.sol" or "MyContract.sol:MyContract" or artifact path relative to the project root directory
+     * @param data Encoded call data of an arbitrary function to call during the upgrade process, or empty if no function needs to be called during the upgrade
+     * @param tryCaller Address to use as the caller of the upgrade function. This should be the address that owns the proxy or its ProxyAdmin.
+     */
+    function upgradeProxy(address proxy, string memory contractName, bytes memory data, address tryCaller) internal {
+        Options memory opts;
+        ValidateAndUpgrade.upgradeProxy(proxy, contractName, data, opts, tryCaller);
+    }
+
+    /**
+     * @dev Upgrades a beacon to a new implementation contract.
+     *
+     * Requires that either the `referenceContract` option is set, or the new implementation contract has a `@custom:oz-upgrades-from <reference>` annotation.
+     *
+     * @param beacon Address of the beacon to upgrade
+     * @param contractName Name of the new implementation contract to upgrade to, e.g. "MyContract.sol" or "MyContract.sol:MyContract" or artifact path relative to the project root directory
+     * @param opts Common options
+     */
+    function upgradeBeacon(address beacon, string memory contractName, Options memory opts) internal {
+        ValidateAndUpgrade.upgradeBeacon(beacon, contractName, opts);
+    }
+
+    /**
+     * @dev Upgrades a beacon to a new implementation contract.
+     *
+     * Requires that either the `referenceContract` option is set, or the new implementation contract has a `@custom:oz-upgrades-from <reference>` annotation.
+     *
+     * @param beacon Address of the beacon to upgrade
+     * @param contractName Name of the new implementation contract to upgrade to, e.g. "MyContract.sol" or "MyContract.sol:MyContract" or artifact path relative to the project root directory
+     */
+    function upgradeBeacon(address beacon, string memory contractName) internal {
+        Options memory opts;
+        ValidateAndUpgrade.upgradeBeacon(beacon, contractName, opts);
+    }
+
+    /**
+     * @notice For tests only. If broadcasting in scripts, use the `--sender <ADDRESS>` option with `forge script` instead.
+     *
+     * @dev Upgrades a beacon to a new implementation contract.
+     *
+     * Requires that either the `referenceContract` option is set, or the new implementation contract has a `@custom:oz-upgrades-from <reference>` annotation.
+     *
+     * This function provides an additional `tryCaller` parameter to test an upgrade using a specific caller address.
+     * Use this if you encounter `OwnableUnauthorizedAccount` errors in your tests.
+     *
+     * @param beacon Address of the beacon to upgrade
+     * @param contractName Name of the new implementation contract to upgrade to, e.g. "MyContract.sol" or "MyContract.sol:MyContract" or artifact path relative to the project root directory
+     * @param opts Common options
+     * @param tryCaller Address to use as the caller of the upgrade function. This should be the address that owns the beacon.
+     */
+    function upgradeBeacon(
+        address beacon,
+        string memory contractName,
+        Options memory opts,
+        address tryCaller
+    ) internal {
+        ValidateAndUpgrade.upgradeBeacon(beacon, contractName, opts, tryCaller);
+    }
+
+    /**
+     * @notice For tests only. If broadcasting in scripts, use the `--sender <ADDRESS>` option with `forge script` instead.
+     *
+     * @dev Upgrades a beacon to a new implementation contract.
+     *
+     * Requires that either the `referenceContract` option is set, or the new implementation contract has a `@custom:oz-upgrades-from <reference>` annotation.
+     *
+     * This function provides an additional `tryCaller` parameter to test an upgrade using a specific caller address.
+     * Use this if you encounter `OwnableUnauthorizedAccount` errors in your tests.
+     *
+     * @param beacon Address of the beacon to upgrade
+     * @param contractName Name of the new implementation contract to upgrade to, e.g. "MyContract.sol" or "MyContract.sol:MyContract" or artifact path relative to the project root directory
+     * @param tryCaller Address to use as the caller of the upgrade function. This should be the address that owns the beacon.
+     */
+    function upgradeBeacon(address beacon, string memory contractName, address tryCaller) internal {
+        Options memory opts;
+        ValidateAndUpgrade.upgradeBeacon(beacon, contractName, opts, tryCaller);
+    }
+
+    /**
+     * @dev Validates an implementation contract, but does not deploy it.
+     *
+     * @param contractName Name of the contract to validate, e.g. "MyContract.sol" or "MyContract.sol:MyContract" or artifact path relative to the project root directory
+     * @param opts Common options
+     */
+    function validateImplementation(string memory contractName, Options memory opts) internal {
+        ValidateAndUpgrade.validateImplementation(contractName, opts);
+    }
+
+    /**
+     * @dev Validates and deploys an implementation contract, and returns its address.
+     *
+     * @param contractName Name of the contract to deploy, e.g. "MyContract.sol" or "MyContract.sol:MyContract" or artifact path relative to the project root directory
+     * @param opts Common options
+     * @return Address of the implementation contract
+     */
+    function deployImplementation(string memory contractName, Options memory opts) internal returns (address) {
+        return ValidateAndUpgrade.deployImplementation(contractName, opts);
+    }
+
+    /**
+     * @dev Validates a new implementation contract in comparison with a reference contract, but does not deploy it.
+     *
+     * Requires that either the `referenceContract` option is set, or the contract has a `@custom:oz-upgrades-from <reference>` annotation.
+     *
+     * @param contractName Name of the contract to validate, e.g. "MyContract.sol" or "MyContract.sol:MyContract" or artifact path relative to the project root directory
+     * @param opts Common options
+     */
+    function validateUpgrade(string memory contractName, Options memory opts) internal {
+        ValidateAndUpgrade.validateUpgrade(contractName, opts);
+    }
+
+    /**
+     * @dev Validates a new implementation contract in comparison with a reference contract, deploys the new implementation contract,
+     * and returns its address.
+     *
+     * Requires that either the `referenceContract` option is set, or the contract has a `@custom:oz-upgrades-from <reference>` annotation.
+     *
+     * Use this method to prepare an upgrade to be run from an admin address you do not control directly or cannot use from your deployment environment.
+     *
+     * @param contractName Name of the contract to deploy, e.g. "MyContract.sol" or "MyContract.sol:MyContract" or artifact path relative to the project root directory
+     * @param opts Common options
+     * @return Address of the new implementation contract
+     */
+    function prepareUpgrade(string memory contractName, Options memory opts) internal returns (address) {
+        return ValidateAndUpgrade.prepareUpgrade(contractName, opts);
+    }
+
+    /**
+     * @dev Gets the admin address of a transparent proxy from its ERC1967 admin storage slot.
+     *
+     * @param proxy Address of a transparent proxy
+     * @return Admin address
+     */
+    function getAdminAddress(address proxy) internal view returns (address) {
+        return ValidateAndUpgrade.getAdminAddress(proxy);
+    }
+
+    /**
+     * @dev Gets the implementation address of a transparent or UUPS proxy from its ERC1967 implementation storage slot.
+     *
+     * @param proxy Address of a transparent or UUPS proxy
+     * @return Implementation address
+     */
+    function getImplementationAddress(address proxy) internal view returns (address) {
+        return ValidateAndUpgrade.getImplementationAddress(proxy);
+    }
+
+    /**
+     * @dev Gets the beacon address of a beacon proxy from its ERC1967 beacon storage slot.
+     *
+     * @param proxy Address of a beacon proxy
+     * @return Beacon address
+     */
+    function getBeaconAddress(address proxy) internal view returns (address) {
+        return ValidateAndUpgrade.getBeaconAddress(proxy);
+    }
+}
