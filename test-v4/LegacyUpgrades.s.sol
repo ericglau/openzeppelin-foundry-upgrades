7484cbb6a031696663d2743726a05b03bfa62c64
Eric Lau <ericglau@outlook.com>
2024-05-06 11:13:48 -0400

    Add v4 tests

--- test-v4/LegacyUpgrades.s.sol
+++ test-v4/LegacyUpgrades.s.sol
@@ -8,7 +8,7 @@ import {GreeterProxiable} from "./contracts/GreeterProxiable.sol";
 import {GreeterV2} from "./contracts/GreeterV2.sol";
 import {GreeterV2Proxiable} from "./contracts/GreeterV2Proxiable.sol";
 
-import {LegacyUpgrades} from "openzeppelin-foundry-upgrades/LegacyUpgrades.sol";
+import {LegacyUpgrades, Options} from "openzeppelin-foundry-upgrades/LegacyUpgrades.sol";
 
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
@@ -17,37 +17,38 @@ import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/Upgradeabl
 import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 
 /**
- * @dev Sample script to deploy and upgrade contracts using transparent, UUPS, and beacon proxies.
+ * @dev Sample script to upgrade OpenZeppelin Contracts v4 deployments using transparent, UUPS, and beacon proxies.
  */
-contract UpgradesScript is Script {
+contract LegacyUpgradesScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        // TODO add validation steps throughout
+        Options memory opts;
+
+        // validate and deploy implementations to be used in proxies
+        address greeter = LegacyUpgrades.deployImplementation("Greeter.sol", opts);
+        address greeterProxiable = LegacyUpgrades.deployImplementation("GreeterProxiable.sol", opts);
 
         // example deployment and upgrade of a transparent proxy
         address proxyAdmin = address(new ProxyAdmin());
-        address transparentProxy = address(new TransparentUpgradeableProxy(address(new Greeter()), proxyAdmin, abi.encodeCall(Greeter.initialize, ("hello"))));
+        address transparentProxy = address(new TransparentUpgradeableProxy(greeter, proxyAdmin, abi.encodeCall(Greeter.initialize, ("hello"))));
         LegacyUpgrades.upgradeProxy(transparentProxy, "GreeterV2.sol", abi.encodeCall(GreeterV2.resetGreeting, ()));
 
-        // // example deployment and upgrade of a UUPS proxy
-        // address uupsProxy = Upgrades.deployUUPSProxy(
-        //     "GreeterProxiable.sol",
-        //     abi.encodeCall(GreeterProxiable.initialize, (msg.sender, "hello"))
-        // );
-        // Upgrades.upgradeProxy(
-        //     uupsProxy,
-        //     "GreeterV2Proxiable.sol",
-        //     abi.encodeCall(GreeterV2Proxiable.resetGreeting, ())
-        // );
+        // example deployment and upgrade of a UUPS proxy
+        address uupsProxy = address(new ERC1967Proxy(
+            greeterProxiable,
+            abi.encodeCall(GreeterProxiable.initialize, ("hello"))
+        ));
+        LegacyUpgrades.upgradeProxy(
+            uupsProxy,
+            "GreeterV2Proxiable.sol",
+            abi.encodeCall(GreeterV2Proxiable.resetGreeting, ())
+        );
 
         // example deployment of a beacon proxy and upgrade of the beacon
-        // address beacon = Upgrades.deployBeacon("Greeter.sol", msg.sender);
-        // Upgrades.deployBeaconProxy(beacon, abi.encodeCall(Greeter.initialize, (msg.sender, "hello")));
-        address impl = address(new Greeter());
-        address beacon = address(new UpgradeableBeacon(impl));
+        address beacon = address(new UpgradeableBeacon(greeter));
         new BeaconProxy(beacon, abi.encodeCall(Greeter.initialize, ("hello")));
         LegacyUpgrades.upgradeBeacon(beacon, "GreeterV2.sol");
 
